deed7aa30e2f1d860c8ef50ffec7a28dcd140c07
Read Me <alex@Alexanders-MBP.fritz.box>
2016-06-22 18:06:25 +0200

    Style Update + Navbar + Language change

--- themes/hugo-agency-theme/static/js/cbpAnimatedHeader.js
+++ themes/hugo-agency-theme/static/js/cbpAnimatedHeader.js
@@ -34,6 +34,7 @@ var cbpAnimatedHeader = (function() {
 
 	function scrollPage() {
 		var sy = scrollY();
+    console.log(scrollY());
     // shows the navbar only when it is scrolled more than the value in 'changeHeaderOn'
 		if ( sy >= changeHeaderOn ) {
 			classie.add( header, 'navbar-shrink' );
